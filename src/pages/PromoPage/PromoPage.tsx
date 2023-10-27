230262f367f78ae78a61cdfffbba60354b2dc7ce
zang3tsu88 <maximenya.a@gmail.com>
2023-10-28 00:19:23 +0300

    feature: add basic modal container

--- src/pages/PromoPage/PromoPage.tsx
+++ src/pages/PromoPage/PromoPage.tsx
@@ -5,6 +5,7 @@ import iconGroup from "../../assets/icons/iconGroup.svg";
 import iconManVoice from "../../assets/icons/iconManVoice.svg";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
+import ResumeModal from "../ResumeModal/ResumeModal";
 
 const PromoPage: React.FC = () => {
   const promoPageTitle = "Яндекс Найм";
@@ -48,6 +49,7 @@ const PromoPage: React.FC = () => {
             <Button type="button" size="lg" className="promoPage__button">
               {promoPageBtnLabel}
             </Button>
+            <ResumeModal />
           </div>
         </div>
         <div className="promoPage__container">
